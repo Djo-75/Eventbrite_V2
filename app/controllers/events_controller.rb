9fe8f622fb2efb7503eebec1ce91ff406db02bd4
Djo-75 <j.pecquet@live.fr>
2023-02-22 00:50:42 +0100

    form event mais ne save pas

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  # before_action :authenticate_user
+  before_action :authenticate_user!
 
   def index
     @all_events = Event.all
@@ -7,7 +7,25 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new('start_date' => params[:start_date], 'duration' => params[:duration], 'title' => params[:title], 'description' => params[:description], 'price' => params[:price], 'location' => params[:location])
+  end
 
+  def create
+    @event = Event.new('start_date' => params[:start_date], 'duration' => params[:duration], 'title' => params[:title], 'description' => params[:description], 'price' => params[:price], 'location' => params[:location])
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    puts "*********"
+    puts params[:start_date]
+    puts "*********"
+    puts params[:duration]
+    puts "*********"
+    puts params[:description]
+    puts "*********"
+    puts params[:location]
+    puts "*********"
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "events#index"
 
-
   get 'users/show'
-  get 'events/index'
-  get 'events/new'
-  get 'events/show'
+
   get 'static_pages/index'
   get 'static_pages/secret'
+
   devise_for :users
+
+  resources :events, only: [:index, :show, :new, :create]
+  
   get 'users/:id' => 'users#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
