b862b8cf62696137478f16e6f7c78e08b93de1cc
Djo-75 <j.pecquet@live.fr>
2023-02-21 19:37:17 +0100

    profil user fait

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
+
+  # Defines the root path route ("/")
+  root "events#index"
+
+
   get 'users/show'
   get 'events/index'
   get 'events/new'
   get 'events/show'
-  get 'event/index'
-  get 'event/new'
-  get 'event/show'
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
-  get 'events/index'
+  get 'users/:id' => 'users#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "static_pages#index"
+
 end
