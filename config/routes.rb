3fae241fe6ace84dcddadd996f50f9d218e84f99
Djo-75 <j.pecquet@live.fr>
2023-02-22 18:53:05 +0100

    test ok page paiement stripe

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,10 @@ Rails.application.routes.draw do
   get 'users/:id' => 'users#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  scope '/checkout' do
+    post 'create' => 'checkout#create', as: 'checkout_create' 
+    get 'success' => 'checkout#success', as: 'checkout_success'
+    get 'cancel' => 'checkout#cancel', as: 'checkout_cancel'
+  end
 
 end
