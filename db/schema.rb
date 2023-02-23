90f1f7ae41dabe63ad427beb9790e5f5f9457748
Djo-75 <j.pecquet@live.fr>
2023-02-23 17:55:32 +0100

    resolving asset pipeline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_152644) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_164058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_152644) do
     t.datetime "updated_at", null: false
     t.bigint "admin_id"
     t.index ["admin_id"], name: "index_events_on_admin_id"
+  end
+
+  create_table "testings", force: :cascade do |t|
+    t.string "username"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
